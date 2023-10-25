62526ab6261e10dfb5bdb3050120557ae7cbf167
Samuel <samuel@twinhealth.com>
2023-10-24 20:27:20 -0700

    create component for cms

--- components/landing/nav.jsx
+++ components/landing/nav.jsx
@@ -4,12 +4,12 @@ export default function LandingNav({ block, dataBinding }) {
     return (
         <section className="nav-bar" data-cms-bind={dataBinding}>
             <div className="logo-container">
-                <img alt="" lassName="logo bcn-logo"
-                    src="https://assets-global.website-files.com/61bb94aac2044c184bd14b46/64c2ef6853bfe9e1f0e78f83_BCN_logo%20wo%20dislcaimer_all%20blue_rgb%20(1).png" />
+                <img alt="" className="logo bcn-logo"
+                    src={block.partnerLogo}/>
                 <img alt="" className="logo"
                     src="https://assets-global.website-files.com/61bb94aac2044c184bd14b46/64c2f18b3a6f4b4cce3a0db0_Twin%20Health%20Primary%20Logo%20(4).png" />
             </div>
-            <div className="nav-headline">Life without Type 2 diabetes is possible</div>
+            <div className="nav-headline">{block.headline}</div>
             <LandingCta></LandingCta>
         </section>
     );
--- content/pages/landing.md
+++ content/pages/landing.md
@@ -10,6 +10,8 @@ seo:
   no_index: false
 content_blocks:
   - _bookshop_name: landing/nav
+    partnerLogo: https://assets-global.website-files.com/61bb94aac2044c184bd14b46/64c2ef6853bfe9e1f0e78f83_BCN_logo%20wo%20dislcaimer_all%20blue_rgb%20(1).png
+    headline: Life without Type 2 diabetes is possible
   - _bookshop_name: landing/hero
   - _bookshop_name: landing/explanation
   - _bookshop_name: landing/contact
