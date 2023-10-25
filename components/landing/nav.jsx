d7e351d97b05e1185b9d885ad76fe1a3961f0694
Samuel <samuel@twinhealth.com>
2023-10-24 20:10:57 -0700

    remove css

--- components/landing/nav.jsx
+++ components/landing/nav.jsx
@@ -9,7 +9,7 @@ export default function LandingNav({ block, dataBinding }) {
                 <img className="logo"
                     src="https://assets-global.website-files.com/61bb94aac2044c184bd14b46/64c2f18b3a6f4b4cce3a0db0_Twin%20Health%20Primary%20Logo%20(4).png" />
             </div>
-            <div className="nav-headline">Life without Type 2 diabetes is possible {block.title}</div>
+            <div className="nav-headline">Life without Type 2 diabetes is possible</div>
             <LandingCta></LandingCta>
         </section>
     );
